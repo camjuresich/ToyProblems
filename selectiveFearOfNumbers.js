41eb0cccae58e163a8b8b9c2d1c4660392189561
Cameron Juresich <camjuresich@gmail.com>
2023-08-18 12:28:41 -1000

    finished writing function, need to write tests

--- selectiveFearOfNumbers.js
+++ selectiveFearOfNumbers.js
@@ -22,18 +22,19 @@ so it tells the doctor if I'm afraid or not. (return a boolean)
 const AmIAfraid = function(day, num){
     switch (day) {
         case "Sunday":
-            return 666 === Math.abs(num) ? true : false
+            return 666 === Math.abs(num) ? true : false;
         case "Monday":
-
+            return num === 12 ? true : false;
         case "Tuesday":
-
+            return num > 95 ? true : false;
         case "Wednesday":
-
+            return num === 34 ? true : false;
         case "Thursday":
-
+            return num === 0 ? true : false;
         case "Friday":
-
+            return num % 2 === 0 ? true : false;
         case "Saturday":
+            return num === 56 ? true : false;
     }
 
   }
