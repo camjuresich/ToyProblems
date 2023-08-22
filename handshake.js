1cac212b952695a1dd515ddabd033c53030a943f
Cameron Juresich <camjuresich@gmail.com>
2023-08-21 22:36:35 -1000

    created handshake need to solve

---
+++ handshake.js
@@ -0,0 +1,31 @@
+/*
+Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+
+Every year he takes photos of farmers handshaking. 
+Johnny knows that no two farmers handshake more than once. 
+He also knows that some of the possible handshake combinations may not happen.
+
+However, Johnny would like to know the minimal amount of people that participated 
+this year just by counting all the handshakes.
+
+Help Johnny by writing a function,
+that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes 
+(a pair of farmers handshake only once). */
+
+function getParticipants(handshakes){
+    const persons = [];
+    // you do not know how many people are required
+    // so you start eith an empty array.
+    // if count === hand shakes return a
+    // making persons a number would probably make more sense
+    
+    // persons = 0
+    // while count < persons run loop
+    1 
+    2 1 2 
+    3 - 1 2, 2 3, 1 3 
+    4 - 1 2, 2 3, 3 4, 1 4
+  
+    console.log(ass)
+    return handshakes
+  }
